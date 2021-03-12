87066cf2acceb4ce7384a3c5701f8a0081cd5bbe
unknown <leomardsr2004@gmail.com>
2021-03-12 12:54:37 -0430

    python

--- python/Registro_Personas_v4.py
+++ python/Registro_Personas_v4.py
@@ -1,5 +1,7 @@
 print ("Bienvenido. Para hacer su registro rellene el formulario: ")
-print ("")
+#print ("")
+
+a=True
 
 class Personas():
 	nombre = ""
@@ -9,6 +11,7 @@ class Personas():
 	correo = ""
 
 	def Registro(self):
+		print("")
 		self.nombre = input ( "Nombres: " )
 		self.apellido = input ("Apellidos: ")
 		self.cedula = input ("Cedula: ")
@@ -23,3 +26,31 @@ class Personas():
 		print ("Nombres: ", self.nombre, "\nApellidos: ", self.apellido, "\nCedula: ", self.cedula, "\nTelefono: ",
 			self.telefono, "\nCorreo: ", self.correo)
 		print("")
+
+
+while a:
+
+	person=Personas()
+	person.Registro()
+	person.Impresion()
+	print("\nDesea registrar a alguien mas?")
+	try:
+		b=str(input("Sí su respuesta es 'Si' escriba 'yes', sino escriba 'no' "))
+	except:
+		print ("Respuesta invalida. el programa ha finalizado")
+
+	b.lower()
+
+	if b == "yes":
+		a=True
+		continue
+
+	elif b == "no":
+		print ("Perfecto, su registro ha sido exitoso. \nEl programa ha FINALIZADO.")
+		break;
+
+	else:
+		print("El Caracter ingresado es incorrecto. programa FINALIZADO.")
+		break;
+
+print("-----------------------------------------------------------------------------------------------------------")
