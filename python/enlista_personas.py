b9670c53c940cbf8c01583c2184a3a53418a8f7b
William Páez <paez.william8@gmail.com>
2021-03-12 20:48:29 -0400

    Arregla el caracter tab a 4 esapcios

--- python/enlista_personas.py
+++ python/enlista_personas.py
@@ -1,61 +1,62 @@
 print ("Bienvenido. Para hacer su registro rellene el formulario: ")
 
+
 def elementos():
-	pass
+    pass
 
 
 def menu():
-	print("Seleccione cual de las opciones desea realizar: ")
-	print("1. Registrar")
-	print("2. Eliminar")
-	print("3. Añadir")
-	print("4. Editar")
-	print("5. Ubicar")
-	pass
+    print("Seleccione cual de las opciones desea realizar: ")
+    print("1. Registrar")
+    print("2. Eliminar")
+    print("3. Añadir")
+    print("4. Editar")
+    print("5. Ubicar")
+    pass
 
-	"""if opcion<1 or opcion>5 or opcion==str():
-		return "Cantidad inválida. Introduzca uno de los numeros que se indican."
+    """if opcion<1 or opcion>5 or opcion==str():
+        return "Cantidad inválida. Introduzca uno de los\
+        numeros que se indican."
 
-	elif opcion == 1:"""
+    elif opcion == 1:"""
 
 
 lista_personas = []
 
+
 class Personas():
-	nombre = ""
-	apellido = ""
-	cedula = ""
-	telefono = ""
-	correo = ""
+    nombre = ""
+    apellido = ""
+    cedula = ""
+    telefono = ""
+    correo = ""
 
-	def Registro(self):
-		print("")
-		self.nombre = input ( "Nombres: " )
-		self.apellido = input ("Apellidos: ")
-		self.cedula = input ("Cedula: ")
-		self.telefono = input ("Telefono:")
-		self.correo = input ("Correo: ")
-		
+    def Registro(self):
+        print("")
+        self.nombre = input("Nombres: ")
+        self.apellido = input("Apellidos: ")
+        self.cedula = input("Cedula: ")
+        self.telefono = input("Telefono:")
+        self.correo = input("Correo: ")
 
-	def Listado(self):
-		print("")
-		lista_personas.append("---------")
-		lista_personas.append(self.nombre)
-		lista_personas.append(self.apellido)
-		lista_personas.append(self.cedula)
-		lista_personas.append(self.telefono)
-		lista_personas.append(self.correo)
-		
-	
-	def Impresion(elemento):
-		
-		print("")
-		print("Personas registradas: ")
-		for i in lista_personas:
-			yield i
-		print(i)
+    def Listado(self):
+        print("")
+        lista_personas.append("---------")
+        lista_personas.append(self.nombre)
+        lista_personas.append(self.apellido)
+        lista_personas.append(self.cedula)
+        lista_personas.append(self.telefono)
+        lista_personas.append(self.correo)
 
-persona=Personas()
+    def Impresion(elemento):
+        print("")
+        print("Personas registradas: ")
+        for i in lista_personas:
+            yield i
+            print(i)
+
+
+persona = Personas()
 persona.Registro()
 persona.Listado()
 persona.Impresion()
